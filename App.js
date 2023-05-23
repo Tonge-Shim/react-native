deab0abd7aca24966f42b75668164ea9000b4e32
Tonge-Shim <tonge20@naver.com>
2023-05-23 13:53:02 +0900

    clear app.js

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Tonge');
-  const [person, setPerson] = useState({ name: 'Ggamja', age: 3});
-  const clickHandler = () => {
-    setName('choonsik');
-    setPerson({ name: 'Ryan', age: 5 });
-  }
+
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler} />
-      </View>
+
     </View>
   );
 }
@@ -26,19 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  buttonContainer: {
-    marginTop: 20,
-  },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-     fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
   },
 });
