a462649b4d2ef764a5b73d3245f204f34916b476
Tonge-Shim <tonge20@naver.com>
2023-05-23 13:11:11 +0900

    tutorial 6, Lists&ScrollView

--- App.js
+++ App.js
@@ -1,44 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Tonge');
-  const [person, setPerson] = useState({ name: 'Ggamja', age: 3});
-  const clickHandler = () => {
-    setName('choonsik');
-    setPerson({ name: 'Ryan', age: 5 });
-  }
+  const [people, setPeople] = useState([
+    { name: 'tonge', key: '1'},
+    { name: 'chaeeun', key: '2'},
+    { name: 'choonsik', key: '3'},
+    { name: 'suri', key: '4'},
+    { name: 'ggamja', key: '5'},
+    { name: 'sweetpotato', key: '6'},
+    { name: 'potato', key: '7'},
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler} />
-      </View>
+
+    <ScrollView>
+      { people.map(item => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+    </ScrollView>
+
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  buttonContainer: {
-    marginTop: 20,
-  },
-  header: {
+  item: {
+    marginTop: 24, 
+    padding: 30,
     backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-     fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
+    fontSize: 24,
   },
 });
